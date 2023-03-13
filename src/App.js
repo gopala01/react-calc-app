83c3766c0a7972d007878d7d9b65d9ed4dfaefa8
gopala01 <bhamidipatig@gmail.com>
2023-03-13 21:44:23 +0000

    commit operator buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ function App() {
 		<div className="App">
 			<div className = "calculator">
 				<div className = "display">
-					<span>(0)</span>0
+					<span>(0)</span> 0
+				</div>
+				<div className="operators">
+					<button>/</button>
+					<button>*</button>
+					<button>+</button>
+					<button>-</button>
+
+					<button>DEL</button>
 				</div>
 			</div>
 		</div>
