fbf4f55863e893f17ea1a2028bd5545cf4f87f16
gopala01 <bhamidipatig@gmail.com>
2023-03-13 21:52:36 +0000

    commit digits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 function App() {
+
+	const createDigits = () => {
+		const digits = [];
+
+		for (let i = 1; i < 10; i++){
+				digits.push(
+					<button key={i}>{i}</button>
+				)
+		}
+
+		return digits
+	}
 	return (
 		<div className="App">
 			<div className = "calculator">
@@ -12,6 +24,12 @@ function App() {
 					<button>-</button>
 
 					<button>DEL</button>
+				</div>
+				<div className="digits">
+					{createDigits()}
+					<button>0</button>
+					<button>.</button>
+					<button>=</button>
 				</div>
 			</div>
 		</div>
